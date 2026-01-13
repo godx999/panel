9607def79f42f870eb89e8b0c8818bdcf466ea48
lvcdy <71778176+lvcdy@users.noreply.github.com>
2026-01-13 11:50:07 +0000

    feat: 优化配置和布局，移除不再使用的图标同步脚本，添加 IP 地址显示功能

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backdropBlur: {
+        xs: '2px',
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.8s ease-in-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 };
